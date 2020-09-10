a08c9813dcabd2f18bf8f898a94b2a7a77c4b71f
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-09-10 16:30:33 +0100

    Finished project.

--- scripts/app.js
+++ scripts/app.js
@@ -1,50 +1,30 @@
 let shareButton = document.querySelector(".user__icon");
-let clickedShareButton = document.querySelector(".shared_icon");
-let unclickedScreen = document.querySelector(".user");
-let clickedScreen = document.querySelector(".share");
-let desktopShare = document.querySelector(".share__desktop");
+let share = document.querySelector(".share");
 let userIcon = document.querySelector(".user__icon i");
-let userIconWrapper = document.querySelector(".user__icon");
-
-let screenSize = window.screen.width * window.devicePixelRatio;
 
 
+// before
+// backgroundColor-- > hsl(210, 46 %, 95 %)
+// user icon color-- > hsl(214, 17 %, 51 %);
 
-const share = (event) => {
-    unclickedScreen.style.display = "none";
-    clickedScreen.style.display = "flex";
-};
+// after
 
-const unshare = (event) => {
-    unclickedScreen.style.display = "flex";
-    clickedScreen.style.display = "none";
-};
+
 
 let state = 0;
 
-// case (state === 0):
-// desktopShare.style.display = "flex";
-// userIcon.style.color = "hsl(210, 46%, 95%)";
-// userIconWrapper.style.backgroundColor = "hsl(210, 46%, 95%)";
-// state++;
-// break;
-//         case (state === 1):
-// desktopShare.style.display = "none";
-// userIcon.style.color = "hsl(214, 17%, 51%)";
-// userIconWrapper.style.backgroundColor = "hsl(214, 17%, 51%);";
-
-const desktopClick = (event) => {
+const toggleShare = (event) => {
     switch (true) {
         case (state === 0):
-            desktopShare.style.display = "flex";
+            share.style.display = "flex";
             userIcon.style.color = "hsl(210, 46%, 95%)";
-            userIconWrapper.style.backgroundColor = "hsl(214, 17%, 51%)";
+            shareButton.style.backgroundColor = "hsl(214, 17%, 51%)";
             state++;
             break;
         case (state === 1):
-            desktopShare.style.display = "none";
+            share.style.display = "none";
             userIcon.style.color = "hsl(214, 17%, 51%)";
-            userIconWrapper.style.backgroundColor = "hsl(210, 46%, 95%)";
+            shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
             state--;
             break;
         default:
@@ -53,17 +33,4 @@ const desktopClick = (event) => {
         }
 }
 
-
-switch (true) {
-    case (screenSize < 1000):
-        shareButton.addEventListener("click", share);
-        clickedScreen.addEventListener("click", unshare);
-        break;
-    case (screenSize >= 1000):
-        shareButton.addEventListener("click", desktopClick);
-        break;
-    default:
-        console.log("Error occured on line 56.")
-    
-        
-}
+shareButton.addEventListener("click", toggleShare);
