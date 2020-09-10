edaba93810051788543890168e2e9e7256a6f0c4
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-09-10 19:04:12 +0100

    Added smooth transition and fixed bugs

--- scripts/app.js
+++ scripts/app.js
@@ -3,26 +3,17 @@ let share = document.querySelector(".share");
 let userIcon = document.querySelector(".user__icon i");
 
 
-// before
-// backgroundColor-- > hsl(210, 46 %, 95 %)
-// user icon color-- > hsl(214, 17 %, 51 %);
-
-// after
-
-
-
 let state = 0;
-
 const toggleShare = (event) => {
     switch (true) {
         case (state === 0):
-            share.style.display = "flex";
+            share.style.opacity = 1;
             userIcon.style.color = "hsl(210, 46%, 95%)";
             shareButton.style.backgroundColor = "hsl(214, 17%, 51%)";
             state++;
             break;
         case (state === 1):
-            share.style.display = "none";
+            share.style.opacity = 0;
             userIcon.style.color = "hsl(214, 17%, 51%)";
             shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
             state--;
